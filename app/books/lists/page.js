f88f013e77a26367143a8fc8cd4594f760ce5885
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-08-13 20:51:07 +0200

    started adding pages do add book list

---
+++ app/books/lists/page.js
@@ -0,0 +1,30 @@
+import { makeList } from '@/lib/list'
+import { getLists } from '@/lib/mongo/list'
+import React from 'react'
+
+export default async function Lists() {
+
+
+    const {lists} = await getLists()
+
+    console.log(lists)
+
+
+  return (
+    <>
+    <div>Lists</div>
+    <ul>
+        {lists.map(list => (<li key={list._id}>{list.name}</li>))}
+    </ul>
+    <br/>
+    <h2>Make List</h2>
+    <form action={makeList}>
+        <label>
+            List Title
+            <input type="text" name="name" placeholder='Add a name'/>
+        </label>
+    </form>
+
+    </>
+  )
+}
--- app/books/search/page.js
+++ app/books/search/page.js
@@ -96,6 +96,9 @@ export default function BookSearch() {
     }
 
 
+    
+
+
   return (
     <>
       <div>BookSearch</div>
