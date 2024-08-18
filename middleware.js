9273e9a2c92bd6ccbdc44996c66512e1b31ae10e
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-08-18 10:34:21 +0200

    Added Paths to Middleware

--- middleware.js
+++ middleware.js
@@ -2,19 +2,20 @@ import { updateSession } from "./lib/auth/udpateSession";
 import { getSession } from "./lib/auth/tokenHandler";
 import { NextResponse } from "next/server";
 
-const protectedRoutes = ['/secret']
+const protectedRoutes = ['/secret', "/books/search", "/books/lists"]
 
 export async function middleware(request) {
   
   if(!await getSession() && protectedRoutes.includes(request.nextUrl.pathname)){
+
+    // TODO: Split URL be be able to block base part
+
     const absoluteURrl = new URL("/", request.nextUrl.origin);
     return NextResponse.redirect(absoluteURrl.toString())
   }
   
   
   return await updateSession(request);
-
-
 
 }
 
