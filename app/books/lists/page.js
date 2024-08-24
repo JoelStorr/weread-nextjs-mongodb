9d83e64130e2bcca7885ac11e0dc34e5aa07a2cc
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-08-24 17:45:26 +0200

    Clean up

--- app/books/lists/page.js
+++ app/books/lists/page.js
@@ -1,65 +1,58 @@
-'use client'
+"use client";
 import { useFormState } from "react-dom";
-import { makeList, getLists, booksFromList, updateProgress } from '@/lib/list'
-import React, { useEffect, useState } from 'react'
+import { makeList, getLists, booksFromList, updateProgress } from "@/lib/list";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
-
 export default function Lists() {
+  const [lists, setLists] = useState(null);
+  const [activeList, setActiveList] = useState(null);
+  const [activeListBooks, setActiveListBooks] = useState(null);
 
+  const [state, formAction] = useFormState(makeList, {
+    error: null,
+    lists: [],
+  });
 
-    const [lists, setLists] = useState(null);
-    const [activeList, setActiveList] = useState(null)
-    const [activeListBooks, setActiveListBooks] = useState(null);
+  useEffect(() => {
+    let load = async () => {
+      let listData = await getLists();
+      console.log("Lists", listData);
+      if (listData) {
+        setLists(listData);
+      }
+    };
 
-    const [state, formAction] = useFormState(makeList, {
-      error: null,
-      lists: [],
+    load();
+  }, []);
+
+  useEffect(() => {
+    let load = async () => {
+      let listData = await getLists();
+      setLists(listData);
+    };
+
+    load();
+  }, [state]);
+
+  async function onActiveList(list) {
+    setActiveList(list);
+
+    let booksIds = list.books.map((book) => book.bookId);
+
+    console.log(booksIds);
+    const result = await booksFromList(booksIds);
+
+    const convertedList = list.books.map((book) => {
+      book.book = result.find((b) => b._id === book.bookId);
+
+      return book;
     });
 
-    useEffect(()=>{
-      let load = async ()=>{
-        let listData = await getLists();
-        console.log('Lists', listData)
-        if(listData){
-          setLists(listData);
-        }
-      }
-
-      load();
-    }, [])
-
-    useEffect(() => {
-      let load = async () => {
-        let listData = await getLists();
-        setLists(listData);
-      };
-
-      load();
-    }, [state]);
-
-    
-    async function onActiveList(list){
-      setActiveList(list);
-
-      let booksIds = list.books.map(book => book.bookId);
-      
-      console.log(booksIds);
-      const result = await booksFromList(booksIds);
-
-      const convertedList = list.books.map((book) => {
-
-        book.book = result.find(b => b._id === book.bookId)
-        
-        return book
-
-      })
-
-      console.log('Converted List',convertedList)
-      setActiveListBooks(convertedList)
-      //TODO: Fix bug loading books from lits
-    }
-
+    console.log("Converted List", convertedList);
+    setActiveListBooks(convertedList);
+    //TODO: Fix bug loading books from lits
+  }
 
   return (
     <>
@@ -83,40 +76,44 @@ export default function Lists() {
       <br />
       <h3>Lists</h3>
       <ul>
-        {lists && lists.map((list) => (
-          <li key={list.name} onClick={() => onActiveList(list)}>
-            {list.name}
-          </li>
-        ))}
+        {lists &&
+          lists.map((list) => (
+            <li key={list.name} onClick={() => onActiveList(list)}>
+              {list.name}
+            </li>
+          ))}
       </ul>
       <br />
       <ul>
         {activeListBooks &&
           activeListBooks.map((el) => (
             <li key={el.book._id}>
-            <p>
-            {el.book.title}
-            </p>
-            {el.book.cover && (
-            <img src={el.book.cover} alt={"Book Cover"} fill/>
-            )}
-            {!el.book.cover && <p>No Cover</p>}
+              <p>{el.book.title}</p>
+              {el.book.cover && (
+                <img src={el.book.cover} alt={"Book Cover"} fill />
+              )}
+              {!el.book.cover && <p>No Cover</p>}
 
-            <p>Current Progress: {el.progress} {el.percent && "%"}</p>
-            <form action={updateProgress}>
-              <label>
-                Update Progress
-                <input type="number" name="progress" defaultValue={el.progress}/>
-              </label>
+              <p>
+                Current Progress: {el.progress} {el.percent && "%"}
+              </p>
+              <form action={updateProgress}>
+                <label>
+                  Update Progress
+                  <input
+                    type="number"
+                    name="progress"
+                    defaultValue={el.progress}
+                  />
+                </label>
 
-              <input type="hidden" value={el.book._id} name="bookId" />
-              <input type="hidden" value={activeList.name} name="listName"/>
-              <input type="hidden" value={el.book.pages} name="pages" />
+                <input type="hidden" value={el.book._id} name="bookId" />
+                <input type="hidden" value={activeList.name} name="listName" />
+                <input type="hidden" value={el.book.pages} name="pages" />
 
-              <button type="submit">Update</button>
-            </form>
-            <p>TotalPages: {el.pages}</p>
-
+                <button type="submit">Update</button>
+              </form>
+              <p>TotalPages: {el.pages}</p>
             </li>
           ))}
       </ul>
