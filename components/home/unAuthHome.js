f86a52421e76c49f276300f07d309f8b342d69c5
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-08-29 22:04:25 +0200

    Added quote Section

--- components/home/unAuthHome.js
+++ components/home/unAuthHome.js
@@ -1,14 +1,12 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
-import classes from './unAuthHome.component.scss';
+import classes from "./unAuthHome.component.scss";
 
 export default function UnAuthHome() {
-
-
-
   return (
     <>
+      {/* NOTE: Header */}
       <div className="join-now-container">
         <img
           src="/man-stands-and-leans-forward.png"
@@ -31,43 +29,51 @@ export default function UnAuthHome() {
       </div>
       <img src="/woman-reading-a-book.png" className="header-image-two" />
 
-
-        <div className='cards-bg'></div>
-
-      <section className="card-holder">
-
-
-
-        <div className="card card-one">
-        <div>
-          <h3>Track</h3>
-          <img src="/icons/Journey.png" />
-        </div>
-          <p>
-            Track your progress with detailed statistics and stay on track with
-            your reading goals.
-          </p>
-        </div>
-        <div className="card card-two">
-        <div>
-          <h3>Connect</h3>
-          <img src="/icons/Connect.png" />
-        </div>
-          <p>
-            Find the perfect group for your next read. Start your own reading
-            club.
-          </p>
-        </div>
-        <div className="card card-three">
-          <div>
-          <h3>Share</h3>
-          <img src="/icons/Open Book.png" />
+      {/* NOTE: Home cards */}
+      <section className="card-section">
+        <div className="card-holder">
+          <div className="card card-one">
+            <div>
+              <h3>Track</h3>
+              <img src="/icons/Journey.png" />
+            </div>
+            <p>
+              Track your progress with detailed statistics and stay on track
+              with your reading goals.
+            </p>
           </div>
-          <p>
-            Your perfect read can be an inspiration for others. Share your books
-            & find your next read.
-          </p>
+          <div className="card card-two">
+            <div>
+              <h3>Connect</h3>
+              <img src="/icons/Connect.png" />
+            </div>
+            <p>
+              Find the perfect group for your next read. Start your own reading
+              club.
+            </p>
+          </div>
+          <div className="card card-three">
+            <div>
+              <h3>Share</h3>
+              <img src="/icons/Open Book.png" />
+            </div>
+            <p>
+              Your perfect read can be an inspiration for others. Share your
+              books & find your next read.
+            </p>
+          </div>
         </div>
+      </section>
+
+      {/* NOTE: Features */}
+      <section className="feature-section">
+        <h1>Todo</h1>
+      </section>
+
+      {/* NOTE: Quote */}
+      <section className="quote-section">
+        <p>“Reading is to the mind what exercise is to the body.”</p>
+        <p>Joseph Addison</p>
       </section>
     </>
   );
