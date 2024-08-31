bbb48ba1506dc47e683088c1ba4759ce2c72e8de
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-08-31 11:43:39 +0200

    Fixed scoping error

--- components/home/authHome.js
+++ components/home/authHome.js
@@ -1,33 +1,23 @@
 import React from 'react'
-import classes from './authHome.component.scss';
+import classes from './authHome.module.scss';
 import CurrentReads from './currentReads/currentReads';
+import ReadingGoals from './readingGoals/readingGoals';
 
 export default function AuthHome() {
   return (
-
     <>
-        <div className='current-reads'>
-            <CurrentReads />
-        </div>
-        <div className='reading-goals'>
-            Reading Goals
-        </div>
-        <div className='reading-list'>
-            Reading List
-        </div>
+      <div className={classes.currentReads}>
+        <CurrentReads />
+      </div>
+      <div className={classes.readingGoals}>
+        <ReadingGoals />
+      </div>
+      <div className={classes.readingList}>Reading List</div>
 
-        <div className='for-you-list'>
-            For You List
-        </div>
+      <div className={classes.forYouList}>For You List</div>
 
-        <div className='your-groupes'>
-            Your Groupes
-        </div>
-        <div className='placeholder-todo-one'>
-            todo
-        </div>
-
+      <div className={classes.yourGroupes}>Your Groupes</div>
+      <div className={classes.placeholderTodoOne}>todo</div>
     </>
-
-  )
+  );
 }
---
+++ components/home/readingGoals/readingGoals.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './readingGoals.component.scss';
+
+export default function ReadingGoals() {
+  return (
+    <>
+        <section className=''>
+            <div>
+                <p>2024</p>
+                <img src="" />
+                <p>Reading <br/> Goals</p>
+            </div>
+            <div >
+                <div className='progress'>
+                    50%
+                </div>
+                <p><span>25</span> of <span>50</span> books</p>
+                <button>Update</button>
+            </div>
+        </section>
+    </>
+  )
+}
