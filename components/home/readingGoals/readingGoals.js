2bccf9cd335aa7dc6f51878d3a880b8fb703cf8e
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-08-31 14:14:12 +0200

    Added year goal element

--- components/home/readingGoals/readingGoals.js
+++ components/home/readingGoals/readingGoals.js
@@ -1,23 +1,72 @@
-import React from 'react';
+'use client'
+import React, {useEffect, useState} from 'react';
 import classes from './readingGoals.module.scss';
 
 export default function ReadingGoals() {
+
+    //const circle = useRef()
+    const radius = 30
+    var circumference = radius * 2 * Math.PI;
+    const [circleOffset, setCircleOffset] = useState(`${circumference}`);
+    
+
+    let strokeDasharray = `${circumference} ${circumference}`;
+    
+
+    function setProgress(percent = 50) {
+      const offset = circumference - (percent / 100) * circumference;
+      //circle.style.strokeDashoffset = offset;
+      setCircleOffset(offset)
+    }
+
+
+   useEffect(()=>{
+    setProgress();
+   }, [])
+
   return (
     <>
-        <section className=''>
-            <div>
-                <p>2024</p>
-                <img src="" />
-                <p>Reading <br/> Goals</p>
-            </div>
-            <div >
-                <div className='progress'>
-                    50%
-                </div>
-                <p><span>25</span> of <span>50</span> books</p>
-                <button>Update</button>
-            </div>
-        </section>
+      <section className={classes.readingGoals}>
+        <div className={classes.yearGoal}>
+          <p>2024</p>
+          <img src="/icons/OpenBook100.png" />
+          <p>
+            Reading <br /> Goals
+          </p>
+        </div>
+        <div>
+          <div className={classes.progress}>
+            <svg class={classes.progressRing} width="120" height="120">
+              <circle
+                class={classes.progressRingCircle}
+                stroke="#2B7CE6"
+                strokeOpacity="50%"
+                stroke-width="8"
+                fill="transparent"
+                r={radius}
+                cx="60"
+                cy="60"
+              />
+              <circle
+                class={classes.progressRingCircle}
+                stroke="#1E2362"
+                stroke-width="8"
+                strokeDasharray={strokeDasharray}
+                strokeDashoffset={circleOffset}
+                fill="transparent"
+                r={radius}
+                cx="60"
+                cy="60"
+              />
+            </svg>
+            <span className={classes.percent}>50%</span>
+          </div>
+          <p>
+            <span>25</span> of <span>50</span> books
+          </p>
+          <button>Update</button>
+        </div>
+      </section>
     </>
-  )
+  );
 }
