fe011014b651273b8219b503ddb8f4fbaeb80339
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-01 10:30:26 +0200

    Load lists from User

--- components/home/readingLists/readingLists.js
+++ components/home/readingLists/readingLists.js
@@ -1,25 +1,18 @@
-import React from 'react'
-import classes from './readingLists.module.scss';
-import { getLists } from '@/lib/list';
+import React from "react";
+import classes from "./readingLists.module.scss";
+import { getLists } from "@/lib/list";
 
 export default async function ReadingLists() {
-
-  const lists = await getLists()
+  const lists = await getLists();
 
   return (
     <>
-        <section className={classes.readingLists}>
-            <h3>Your Lists</h3>
-            <ul>
-               {lists &&
-          lists.map((list) => (
-            <li key={list.name}>
-              {list.name}
-            </li>
-          ))}
-            </ul>
-        </section>
+      <section className={classes.readingLists}>
+        <h3>Your Lists</h3>
+        <ul>
+          {lists && lists.map((list) => <li key={list.name}>{list.name}</li>)}
+        </ul>
+      </section>
     </>
-
-  )
+  );
 }
