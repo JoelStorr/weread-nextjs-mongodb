a228f6956eeb64092d38507e8aae44404c2e881d
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-02 16:16:45 +0200

    Removed unused Movie file

--- app/page.js
+++ app/page.js
@@ -1,31 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { getMovies } from "./../lib/mongo/movies.js";
-import Link from "next/link";
-import AuthContextProvider from "@/store/auth-context";
 import AuthPopUp from "@/components/auth/authPopup";
 import { getSession } from "@/lib/auth/tokenHandler";
 import UnAuthHome from "@/components/home/unAuthHome";
 import AuthHome from "@/components/home/authHome";
 
-async function fetchMovies() {
-  const { movies } = await getMovies();
-  if (!movies) throw new Error("Failed to fetch Movies");
-
-  return movies;
-}
-
-export default async function Home({searchParams}) {
+export default async function Home({ searchParams }) {
   const session = await getSession();
 
   const login = searchParams.login || false;
   const register = searchParams.register || false;
 
-
-  if(session){
-    return (
-      <AuthHome />
-    )
+  if (session) {
+    return <AuthHome />;
   } else {
     return (
       <>
@@ -35,8 +20,4 @@ export default async function Home({searchParams}) {
       </>
     );
   }
-
-
-
-
 }
