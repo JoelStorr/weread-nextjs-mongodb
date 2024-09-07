ca4f374535baca3b2368529f6272828988fd00e5
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-07 11:15:33 +0200

    Added profile files

---
+++ app/profile/editor/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const  EditorPage: React.FC = async () => {
+
+    
+
+
+  return (
+    <div>page</div>
+  )
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import classes from './profile.module.scss';
+
+const ProfilePage: React.FC = async ():Promise<React.JSX.Element>=>{
+
+    
+
+    return (
+      <div className={classes.profile}>
+        <div className={classes.profileHeader}>
+          <img
+            src="/profile/profile-header.jpg"
+            className={classes.profileBg}
+          />
+        </div>
+        <div className={classes.profileInfo}>
+          <img src="" className={classes.profileImage} />
+          <div>
+            <div>
+              <span>@username</span>
+              <span>name</span>
+            </div>
+            <div>
+              <h4>Details</h4>
+              <div>
+                <p>
+                  Follower <span>5</span>
+                </p>
+                <p>
+                  Following <span>5</span>
+                </p>
+              </div>
+              <div>
+                <p>Location Germany</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h1>Test Content</h1>
+        </div>
+      </div>
+    );
+}
+
+
+export default ProfilePage;
